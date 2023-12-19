d2e0d21a7c7d4007f8cfea9a3e374a476554d97d
UsefulDust <104992404+UsefulDust@users.noreply.github.com>
2023-12-18 23:39:03 -0500

    1

--- Assets/Scripts/Jeu/Guards/GuardTargetSomeone.cs
+++ Assets/Scripts/Jeu/Guards/GuardTargetSomeone.cs
@@ -76,14 +76,16 @@ public class GuardTargetSomeone : MonoBehaviour
         {
             // Désactiver les commandes et le contrôle de la caméra du joueur
             transform.LookAt(targetPlayerTransform, Vector3.up);
-            transform.rotation = new Quaternion(0, gameObject.transform.rotation.y, 0, 0);
+            //transform.rotation = Quaternion.identity;
+            Camera.main.fieldOfView = 95;
             Camera.main.transform.LookAt(gameObject.transform, Vector3.up);
+            
             collisionScript.Collision(gameObject.name, targetPlayerTransform.tag, navMeshAgent);
         }
         if (Vector3.Distance(gameObject.transform.position, targetOpponentTransform.position) < 2.35f)
         {
-            transform.LookAt(targetOpponentTransform, Vector3.up);     
-            transform.rotation = Quaternion.Euler(0, gameObject.transform.rotation.y, 0);
+            transform.LookAt(targetOpponentTransform, Vector3.up);
+            //transform.rotation = Quaternion.identity;
             collisionScript.Collision(gameObject.name, targetOpponentTransform.tag, navMeshAgent);
         }
     }
